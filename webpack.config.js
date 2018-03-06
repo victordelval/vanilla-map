9e511dd83ed9aa59c2ad7ae1be14e9805deb59f6
victordelval <victor.val.andres@gmail.com>
2018-03-06 16:45:42 +0100

    Completes initial setup

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,17 @@ const config = require('./package.json');
 const webpack = require('webpack');
 require('dotenv').config();
 
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
 const PROD = process.env.NODE_ENV === 'production';
 
 let plugins = [];
 
-PROD ? [
-    plugins.push(new webpack.optimize.UglifyJsPlugin({
-        compress: { warnings: false }
-    }))
-] : '';
+// PROD ? [
+//     plugins.push(new webpack.optimize.UglifyJsPlugin({
+//         compress: { warnings: false }
+//     }))
+// ] : '';
 
 module.exports = {
     entry: path.resolve(__dirname, config.main),
@@ -24,9 +26,48 @@ module.exports = {
         filename: (PROD) ? 'build/vanilla-map.min.js' : 'build/vanilla-map.js'
     },
     module: {
-        loaders: [
-            { test: /\.es6?$/, exclude: /node_modules/, loader: 'babel-loader' }
+        // loaders: [
+        //     { test: /\.es6?$/, exclude: /node_modules/, loader: 'babel-loader' }
+        // ]
+        rules: [
+            // rules for modules (configure loaders, parser options, etc.)
+
+            {
+              test: /\.es6?$/,
+              include: [
+                path.resolve(__dirname, "src")
+              ],
+              exclude: [
+                path.resolve(__dirname, "node_modules")
+              ],
+
+              loader: "babel-loader",
+              // the loader which should be applied, it'll be resolved relative to the context
+              // -loader suffix is no longer optional in webpack2 for clarity reasons
+              // see webpack 1 upgrade guide
+
+              options: {
+                presets: ["es2015"]
+              },
+              // options for the loader
+            }
         ]
     },
-    plugins: plugins
+    plugins: plugins,
+
+    optimization: {
+        minimizer: [
+          // we specify a custom UglifyJsPlugin here to get source maps in production
+          new UglifyJsPlugin({
+            cache: true,
+            parallel: true,
+            uglifyOptions: {
+              compress: false,
+              ecma: 6,
+              mangle: true
+            },
+            sourceMap: true
+          })
+        ]
+      },
 };
