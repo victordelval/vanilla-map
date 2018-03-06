12279c32a2fe38c8a08950b980aaa76875ac2071
victordelval <victor.val.andres@gmail.com>
2018-03-06 23:16:32 +0100

    Cleans config files

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const PROD = process.env.NODE_ENV === 'production';
 
+
 let plugins = [];
 
 // PROD ? [
@@ -25,49 +26,48 @@ module.exports = {
         path: __dirname,
         filename: (PROD) ? 'build/vanilla-map.min.js' : 'build/vanilla-map.js'
     },
+
     module: {
-        // loaders: [
-        //     { test: /\.es6?$/, exclude: /node_modules/, loader: 'babel-loader' }
-        // ]
         rules: [
             // rules for modules (configure loaders, parser options, etc.)
-
             {
-              test: /\.es6?$/,
-              include: [
-                path.resolve(__dirname, "src")
-              ],
-              exclude: [
-                path.resolve(__dirname, "node_modules")
-              ],
+                test: /\.es6?$/,
+                include: [
+                    path.resolve(__dirname, "src")
+                ],
+                exclude: [
+                    path.resolve(__dirname, "node_modules")
+                ],
 
-              loader: "babel-loader",
-              // the loader which should be applied, it'll be resolved relative to the context
-              // -loader suffix is no longer optional in webpack2 for clarity reasons
-              // see webpack 1 upgrade guide
+                loader: "babel-loader",
+                // the loader which should be applied, it'll be resolved relative to the context
+                // -loader suffix is no longer optional in webpack2 for clarity reasons
+                // see webpack 1 upgrade guide
 
-              options: {
-                presets: ["es2015"]
-              },
-              // options for the loader
+                options: {
+                    presets: ["es2015"]
+                },
+                // options for the loader
             }
         ]
     },
+
     plugins: plugins,
 
     optimization: {
         minimizer: [
-          // we specify a custom UglifyJsPlugin here to get source maps in production
-          new UglifyJsPlugin({
-            cache: true,
-            parallel: true,
-            uglifyOptions: {
-              compress: false,
-              ecma: 6,
-              mangle: true
-            },
-            sourceMap: true
-          })
+            // we specify a custom UglifyJsPlugin here to get source maps in production
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                uglifyOptions: {
+                    compress: false,
+                    ecma: 6,
+                    mangle: true
+                },
+                sourceMap: true
+            })
         ]
-      },
+    },
+
 };
